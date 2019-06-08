93c05d95a02ef7044f6ee20b42ed769e43ae683c
Shawn <shawnmkramer@hotmail.com>
2019-06-08 08:59:54 -0400

    initial checkin

---
+++ ConsolePaintProgram/ConsolePaintProgram/ConsolePaintProgram/Brushes/DeltaBrush.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePaintProgram
+{
+    public class DeltaBrush : IBrush
+    {
+        public string Color { get; set; }
+
+        public char GetStroke()
+        {
+            return (char)916;
+        }
+    }
+}
---
+++ ConsolePaintProgram/ConsolePaintProgram/ConsolePaintProgram/Shapes/Line.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePaintProgram
+{
+   public class Line : Shape
+    {
+        public override void Draw()
+        {
+            int length = GetLength();
+            int angle = GetAngle();
+
+            Console.WriteLine();
+            Console.WriteLine("drawing a line");
+            Console.WriteLine();
+
+        }
+
+        private int GetLength()
+        {
+            Console.Write("Enter length: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        private int GetAngle()
+        {
+            Console.Write("Enter angle: ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
