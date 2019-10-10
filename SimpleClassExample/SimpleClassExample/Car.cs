b2fe2f02a431b1e727251a6f80321da824544e1b
Kramer, Shawn <Shawn.Kramer@ClaytonHomes.com>
2019-10-10 14:18:34 -0400

    adding example

---
+++ SimpleClassExample/SimpleClassExample/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        private string _petName;
+
+        public string PetName
+        {
+            get
+            {
+
+                return _petName;
+            }
+            set
+            {
+                _petName = value;
+            }
+        }
+
+        private int _currSpeed;
+        public int CurrSpeed
+        {
+            get
+            {
+                return _currSpeed;
+            }
+            set
+            {
+                _currSpeed = value;
+            }
+        }
+
+
+
+        internal void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPH.", _petName, _currSpeed);
+        }
+
+        internal void SpeedUp(int delta)
+        {
+
+            _currSpeed += delta;
+
+        }
+    }
+}
---
+++ SimpleClassExample/SimpleClassExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            myCar.PetName = "Henry";
+            myCar.CurrSpeed = 10;
+
+            myCar.PrintState();
+
+            for (int i = 0; i < 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+
+            Car[] carArray = new Car[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                carArray[i] = new Car();
+                carArray[i].CurrSpeed = 5 + i;
+                carArray[i].PetName = $"Car{i}";
+            }
+
+            foreach (Car item in carArray)
+            {
+                item.PrintState();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
